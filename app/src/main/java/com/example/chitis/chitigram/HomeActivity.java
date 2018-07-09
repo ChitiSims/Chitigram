80f199a3a2666921e605100b20443d7061bfdc63
Chiti Simbotwe <chitis@fb.com>
2018-07-09 16:47:08 -0700

    staying safe

--- app/src/main/java/com/example/chitis/chitigram/HomeActivity.java
+++ app/src/main/java/com/example/chitis/chitigram/HomeActivity.java
@@ -1,29 +1,63 @@
 package com.example.chitis.chitigram;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.chitis.chitigram.model.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String imagePath
+    private EditText descriptionInput;
+    private Button createButton;
+    private Button refreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        descriptionInput = findViewById(R.id.etDescription);
+        createButton = findViewById(R.id.btCreate);
+        refreshButton = findViewById(R.id.btRefresh);
+
+        loadTopPosts();
+
+    }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        // TODO - create and save post
+    }
+
+    private void loadTopPosts() {
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null) {
+                    for (int i = 0; i < objects.size(); i++)
+                        Log.d("HomeActivity", "Post[" + i + "] = "
+                                + objects.get(i).getDescription()
+                                + "\nusername = " + objects.get(i).getUser().getUsername()
+                        );
+                } else {
+                    e.printStackTrace();
+                }
             }
         });
     }
+
+
 
 }
