03796da9cbdca998b248400b51be9fcbfb61a578
Chiti Simbotwe <chitis@fb.com>
2018-07-11 14:05:34 -0700

    can post picture to parse server

--- app/src/main/java/com/example/chitis/chitigram/HomeActivity.java
+++ app/src/main/java/com/example/chitis/chitigram/HomeActivity.java
@@ -1,25 +1,44 @@
 package com.example.chitis.chitigram;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.chitis.chitigram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String imagePath
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public final String APP_TAG = "MyCustomApp";
+    private static String imagePath;
     private EditText descriptionInput;
     private Button createButton;
     private Button refreshButton;
+    private Button photoButton;
+    private ImageView picture;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +48,57 @@ public class HomeActivity extends AppCompatActivity {
         descriptionInput = findViewById(R.id.etDescription);
         createButton = findViewById(R.id.btCreate);
         refreshButton = findViewById(R.id.btRefresh);
+        photoButton = findViewById(R.id.btphoto);
+        picture = findViewById(R.id.ivPicture);
 
-        loadTopPosts();
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String description = descriptionInput.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+                final File file = new File(String.valueOf(photoFile));
+                final ParseFile parseFile = new ParseFile(file);
+                Toast.makeText(HomeActivity.this, "created", Toast.LENGTH_LONG).show();
+
+                createPost(description, parseFile, user);
+            }
+        });
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadTopPosts();
+            }
+        });
+
+        photoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
 
     }
 
     private void createPost(String description, ParseFile imageFile, ParseUser user) {
-        // TODO - create and save post
+       final Post newPost = new Post();
+       newPost.setDescription(description);
+       newPost.setImage(imageFile);
+       newPost.setUser(user);
+
+       newPost.saveInBackground(new SaveCallback() {
+           @Override
+           public void done(ParseException e) {
+               if (e == null) {
+                   Log.d("HomeActivity", "Create post success!");
+               } else {
+                   e.printStackTrace();
+               }
+           }
+       });
+
     }
 
     private void loadTopPosts() {
@@ -60,4 +123,56 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
-}
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.example.chitis.chitigram", photoFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            String photoFileName = photoFile.getPath();
+            Bitmap bitmap = BitmapFactory.decodeFile(photoFileName);
+
+            picture.setImageBitmap(bitmap);
+
+
+
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+
+    }
+
+
+
+
